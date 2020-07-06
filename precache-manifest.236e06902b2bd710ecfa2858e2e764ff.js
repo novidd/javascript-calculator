e6d756148be50b53095622e6fc9f9761647539f8
Lucas Pettersson <clm.pettersson@gmail.com>
2020-07-06 19:53:06 +0200

    Updates

---
+++ precache-manifest.236e06902b2bd710ecfa2858e2e764ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66e434bb8d443a7c650156e022001fa",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "34093b5e34e56202a73b",
+    "url": "/javascript-calculator/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d5b26deea65467c57a73",
+    "url": "/javascript-calculator/static/js/2.37347c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.37347c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34093b5e34e56202a73b",
+    "url": "/javascript-calculator/static/js/main.633dd4cf.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
